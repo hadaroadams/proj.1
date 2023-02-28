ed64547be0145aeedcbc729e46f525ad35336372
hadaroadams <hadaroadams1234@gmail.com>
2023-02-28 16:40:34 +0000

    some message

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function pass_dect(){
     let password=document.getElementById("password").value
     let mesg
         if (password.length<6){
-            mesg="weak password"
+            mesg="weak passwords"
     }
     else{
         mesg="strong password"
